7ac4df762028154e442125c1f3316d86e510c77e
Wyatt Lindsey <wyatt@newrelic.com>
2016-06-10 12:08:55 -0700

    general cleanup for git ignored files

--- dev/testing/models/apple.js
+++
@@ -1,9 +0,0 @@
-function Apple(id) {
-  this.id = id;
-}
-
-Apple.prototype.getID = function() {
-  return this.id || 0;
-};
-
-module.exports = Apple;
--- dev/testing/models/orange.js
+++
@@ -1,9 +0,0 @@
-function Orange(id) {
-  this.id = id;
-}
-
-Orange.prototype.getID = function() {
-  return this.id || 0;
-};
-
-module.exports = Orange;
